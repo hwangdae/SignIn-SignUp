816e5ea7cf3abd481a700acfc9460179fa80185f
hwangdae <klp2797@naver.com>
2024-04-23 16:34:26 +0900

    feat:다크모드 토글식 구현

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,15 @@
+import { ModeToggle } from "@/components/mode-toggle";
+import { ThemeProvider } from "@/components/theme-provider";
 import "@/styles/globals.css";
-import type { AppProps } from 'next/app'
- 
+import type { AppProps } from "next/app";
+
 export default function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <ThemeProvider attribute="class" defaultTheme="light">
+      <div>
+        <Component {...pageProps} />
+      </div>
+      <ModeToggle className={"absolute top-6 right-6"} />
+    </ThemeProvider>
+  );
 }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import {
   Card,
   CardContent,
@@ -12,27 +12,49 @@ import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
 
 const Home = () => {
+  const [step, setStep] = useState<number>(0);
   return (
-    <Card className="w-[350px]">
-    <CardHeader>
-      <CardTitle>Create project</CardTitle>
-      <CardDescription>Deploy your new project in one-click.</CardDescription>
-    </CardHeader>
-    <CardContent>
-      <form>
-        <div className="grid w-full items-center gap-4">
-          <div className="flex flex-col space-y-1.5">
-            <Label htmlFor="name">Name</Label>
-            <Input id="name" placeholder="Name of your project" />
+    <div className="w-[350px] absolute -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2">
+      <Card className="w-[350px]  bg-gradient-to-b from-red-500 to-orange-500">
+        <CardHeader>
+          <CardTitle className="text-white text-[34px]">
+            Welcome,
+            <br />
+            back
+          </CardTitle>
+        </CardHeader>
+        <CardContent>
+          <div className="flex flex-col gap-2">
+          <Button variant={"outline"}>계정 만들기</Button>
+          <Button variant={"outline"}>로그인</Button>
           </div>
-        </div>
-      </form>
-    </CardContent>
-    <CardFooter className="flex justify-between">
-      <Button variant="outline">Cancel</Button>
-      <Button>Deploy</Button>
-    </CardFooter>
-  </Card>
+        </CardContent>
+      </Card>
+      <Card className="w-[350px]">
+        <CardHeader>
+          <CardTitle>계정 만들기</CardTitle>
+          <CardDescription>
+            Deploy your new project in one-click.
+          </CardDescription>
+        </CardHeader>
+        <CardContent>
+          <form>
+            <div className="grid w-full items-center gap-4">
+              <div className="flex flex-col space-y-1.5">
+                <Input id="email" placeholder="이메일" />
+                <Input id="lastName" placeholder="성" />
+                <Input id="firstName" placeholder="이름" />
+                <Input id="phone" placeholder="휴대폰 번호" />
+              </div>
+            </div>
+          </form>
+        </CardContent>
+        <CardFooter className="flex justify-between">
+          <Button variant="outline">Cancel</Button>
+          <Button>Deploy</Button>
+        </CardFooter>
+      </Card>
+    </div>
   );
 };
 
